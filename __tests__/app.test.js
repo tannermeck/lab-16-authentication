6c35c160f41d6c033c5e9e1595a81ae9278483c8
TANNER MECK <steve.meck246@gmail.com>
2021-10-07 13:53:01 -0700

    first test passing post route to signup

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,9 +8,13 @@ describe('lab-16-authentication routes', () => {
     return setup(pool);
   });
 
-  it('should post a user using /signup route', () => {
-    const user = request(app).post('/api/auth/signup').send({ email: 'tanner@alchemy.com', password: 'password' });
-    expect(user.body).toEqual({ id: expect.any(Number), email: 'tanner@alchemy.com' });
+  it('should post a user using /signup route', async () => {
+    return request(app)
+      .post('/api/auth/signup')
+      .send({ email: 'tanner@alchemy.com', password: 'password' })
+      .then((response) => {
+        expect(response.body).toEqual({ id: expect.any(String), email: 'tanner@alchemy.com' });
+      });
   });
 
   afterAll(() => {
