30fc8847d420026c9eedd5f30a2fbc09e934bf12
TANNER MECK <steve.meck246@gmail.com>
2021-10-07 13:13:11 -0700

    test for signup post route setup and failing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,6 +8,11 @@ describe('lab-16-authentication routes', () => {
     return setup(pool);
   });
 
+  it('should post a user using /signup route', () => {
+    const user = request(app).post('/signup').send({ email: 'tanner@alchemy.com', password: 'password' });
+    expect(user.body).toEqual({ id: expect.any(Number), email: 'tanner@alchemy.com' });
+  });
+  
   afterAll(() => {
     pool.end();
   });
