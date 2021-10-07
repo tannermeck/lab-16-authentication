349176113babc5688cb33082ba2951683d5084ec
TANNER MECK <steve.meck246@gmail.com>
2021-10-07 14:14:40 -0700

    test for signup with an email that already exists

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -16,6 +16,17 @@ describe('lab-16-authentication routes', () => {
         expect(response.body).toEqual({ id: expect.any(String), email: 'tanner@alchemy.com' });
       });
   });
+  it('if the same email already exists, a 400 message is sent', async () => {
+    await request(app)
+      .post('/api/auth/signup')
+      .send({ email: 'tanner@alchemy.com', password: '123' });
+    return request(app)
+      .post('/api/auth/signup')
+      .send({ email: 'tanner@alchemy.com', password: '456' })
+      .then((response) => {
+        expect(response).toEqual(400);
+      });
+  });
 
   afterAll(() => {
     pool.end();
