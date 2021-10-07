99ed76ea1f283108d75c9678dfa42d27b9e2476e
TANNER MECK <steve.meck246@gmail.com>
2021-10-07 13:28:51 -0700

    table and auth route setup

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -9,10 +9,10 @@ describe('lab-16-authentication routes', () => {
   });
 
   it('should post a user using /signup route', () => {
-    const user = request(app).post('/signup').send({ email: 'tanner@alchemy.com', password: 'password' });
+    const user = request(app).post('/api/auth/signup').send({ email: 'tanner@alchemy.com', password: 'password' });
     expect(user.body).toEqual({ id: expect.any(Number), email: 'tanner@alchemy.com' });
   });
-  
+
   afterAll(() => {
     pool.end();
   });
